7ef20d491756aee47119576d483e168e56dbc7c9
smitbhuva15 <itssmit123@gmail.com>
2025-03-07 01:28:49 +0530

    metting all Functionality Done!!

--- src/components/meeting/MeetingRoom.tsx
+++ src/components/meeting/MeetingRoom.tsx
@@ -1,15 +1,29 @@
 "use client"
-import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
-import { useRouter } from 'next/navigation';
+import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useState } from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LayoutList, Users } from 'lucide-react';
+import EndCallBtn from './EndCallBtn';
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 export default function MeetingRoom() {
+
+    const searchParams = useSearchParams();
+    const isPersonalRoom = !!searchParams.get('personal');
+   
     const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
     const [showParticipants, setShowParticipants] = useState(false);
-    const router=useRouter()
- 
+    const router = useRouter()
+
 
     const CallLayout = () => {
         switch (layout) {
@@ -24,18 +38,49 @@ export default function MeetingRoom() {
 
 
     return (
-        <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+        <section className="relative h-screen w-full  overflow-hidden pt-4 text-white">
             <div className="relative flex size-full items-center justify-center">
                 <div className=" flex size-full max-w-[1000px] items-center">
                     <CallLayout />
                 </div>
-                <div  className={`h-[calc(100vh-86px)] hidden ml-2  ${showParticipants&& "show-block"}`  } >
+                <div className={`h-[calc(100vh-86px)] hidden ml-2  ${showParticipants && "show-block"}`} >
                     <CallParticipantsList onClose={() => setShowParticipants(false)} />
                 </div>
             </div>
 
-            <div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
-                <CallControls onLeave={() => router.push(`/`)}/>
+            <div className='fixed bottom-0 flex w-full flex-wrap items-center justify-center gap-5'>
+                <CallControls onLeave={() => router.push(`/`)} />
+
+                <DropdownMenu>
+                    <div className="flex items-center">
+                        <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+                            <LayoutList size={20} className="text-white" />
+                        </DropdownMenuTrigger>
+                    </div>
+
+                    <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+                        {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+                            <div key={index}>
+                                <DropdownMenuItem
+                                    onClick={() =>
+                                        setLayout(item.toLowerCase() as CallLayoutType)
+                                    }
+                                >
+                                    {item}
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator className="border-dark-1" />
+                            </div>
+                        ))}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                 
+                <CallStatsButton />
+                <button onClick={() => setShowParticipants((prev) => !prev)}>
+                    <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+                        <Users size={20} className="text-white " />
+                    </div>
+                </button>
+                {!isPersonalRoom && <EndCallBtn />}
             </div>
         </section>
     )
