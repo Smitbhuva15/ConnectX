4dc3fffb05f4ea2702085e31aa01b6e469267198
smitbhuva15 <itssmit123@gmail.com>
2025-03-06 13:59:49 +0530

    home page UI done!!

---
+++ src/components/HomeCart.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import React from 'react'
+
+type homecartprops = {
+
+  img: string
+  description: string
+  title: string
+  className?: string
+}
+
+export default function HomeCart({ img, description, title, className }: homecartprops) {
+  return (
+    <section
+      className={`${className}  px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer`}
+    >
+      <div className="flex-center glassmorphism size-12 rounded-[10px]">
+        <Image src={img} alt="meeting" width={27} height={27} />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-lg font-normal">{description}</p>
+      </div>
+
+    </section>
+  )
+}
---
+++ src/components/MettingList.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import HomeCart from './HomeCart'
+
+export default function MettingList() {
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+        <HomeCart  
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        className='bg-orange-1'
+        />
+        <HomeCart 
+         img="/icons/join-meeting.svg"
+         title="Join Meeting"
+         description="via invitation link"
+         className="bg-blue-1"/>
+        <HomeCart 
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        className="bg-purple-1"/>
+        <HomeCart 
+          img="/icons/recordings.svg"
+          title="View Recordings"
+          description="Meeting Recordings"
+          className="bg-yellow-1"/>
+
+    </section>
+  )
+}
